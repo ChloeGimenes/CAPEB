d8d15c46cd943c8fbeec2563bc72c44eded1ef11
CHLOE GIMENES <chloegimenes@gmail.com>
2021-03-02 14:47:18 +0100

    navbar ok

--- capeb.js
+++ capeb.js
@@ -1,12 +1,18 @@
    /* NAVBAR ACTIVE LINKS /////////////////////////////////////////////////////////////////////////////////////////////*/
 
    $(document).ready(function(){
-    $('ul li a').click(function(){
-      $('li a').removeClass("active");
+    $('.menu li').click(function(){
+      $('li').removeClass("active");
       $(this).addClass("active");
   });
   });
 
   
 
+  // $(document).ready(function () {
+  
+  //   $(".menu li").click(function (){
+  //     $(".menu li span").addClass("active").siblings().removeClass("active");
+  //   });
+  // });
   

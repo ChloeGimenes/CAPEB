cefcf23e9a97b1ad17ef116a8ea8d8f0242cb250
CHLOE GIMENES <chloegimenes@gmail.com>
2021-03-09 12:49:31 +0100

    home ok & mon parcours 1st jet

--- capeb.js
+++ capeb.js
@@ -12,23 +12,42 @@
 
 /* CALENDAR */
 
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
 
-    dateClick: function(info) {
-      alert('Date: ' + info.dateStr);
-      alert('Resource ID: ' + info.resource.id);
+      dateClick: function(info) {
+        alert('Date: ' + info.dateStr);
+        alert('Resource ID: ' + info.resource.id);
 
-      htmle = 'Date: ' + info.dateStr,
-      document.getElementById('main-container').innerHTML= htmle;
+        htmle = 'Date: ' + info.dateStr,
+        document.getElementById('main-container').innerHTML= htmle;
 
-    }
-    
+      }
+      
+    });
+    calendar.render();
   });
-  calendar.render();
-});
 
 
+  
+/* PAGE PARCOURS MATRIMOINE */
 
+  /* QUESTIONS RADIO BOXES */
+  $("form-check input:radio").change(function () {$("#button-patrimoine").prop("disabled", false);});
+
+
+  /* FORM CONTACT REMOVE DISABLE BUTTON*/
+ 
+  $('#username, #userlastname, #usermail, #userphone, #userphone, #userdept').bind('keyup', function() {
+    if(allFilled()) $("#register").prop('disabled', false);
+    });
+
+    function allFilled() {
+        var filled = true;
+        $('input').each(function() {
+            if($(this).val() == '') filled = false;
+        });
+        return filled;
+    }
